b125ff178f5566306fcaf59cd3ae47b4f605d38b
ashoordoushow <ashoor12323@gmail.com>
2024-08-30 18:36:06 -0500

    testing

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -1,5 +1,5 @@
 class ExamplePagesController < ApplicationController
     def another          #def method is used to print out the action: in routes.rb file
         render json: {message: "hello world"} #this is the syntax used to output your message back to the internet
-    end
+    end   #this is the end statement
 end
