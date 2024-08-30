335093486c7a2791690184fc8b188a16dbb9bbca
ashoordoushow <ashoor12323@gmail.com>
2024-08-30 18:31:26 -0500

    ex. rails app

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "/second_route", controller: "example_pages", action: "another" #'get' means your searching the web, controller: is the method you use to create another file through terminal 'rails generate controller example_pages', and action: is what you label your 'def' method in 'example_pages_controller.rb' file
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
